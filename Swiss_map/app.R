1c9e5be45540a8925798bd38dbe9993f33c5194f
Upsylon <patrik.grandadam@unil.ch>
2018-12-04 17:04:29 +0100

    Initialization Shiny App n2

--- Swiss_map/app.R
+++ Swiss_map/app.R
@@ -1,50 +1,89 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+ui <- library(shiny)
+
+shinyUI(fluidPage(
+  
+  titlePanel("Pi Estimation"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput("method",
+                  "The function to use:",
+                  choices = c("estimate_pi", "estimate_pi2")
+      ),
+      
+      numericInput("seed",
+                   "The desired seed:",
+                   value = 10,
+                   min = 0,
+                   max = Inf
+      ),
+      
+      sliderInput("B",
+                  "Number of simulated points:",
+                  value = 5000,
+                  min = 1,
+                  max = 10^6
+      ),
+      actionButton("button", label = "Apply Changes", icon = NULL, width = NULL
+      )
+    ),
+    
+    mainPanel(
+      
+      plotOutput("plot"),
+      
+      textOutput("time"),
+      
+      textOutput("pi")
+    )
+  )
+)
+)
+
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+server <- shinyServer(function(input, output) {
+  
+  
+  simulate <- eventReactive(input$button, {
+    # simulate pi and measure the time here
+    
+    if (input$method == "estimate_pi") {
+      estimated_pi <- estimate_pi(B = input$B, seed = input$seed)
+    } else {
+      estimated_pi <- estimate_pi2(B = input$B, seed = input$seed)
+    }
+    return(estimated_pi)
+    
+  })
+  
+  simulated_time <- eventReactive(input$button, {
+    if (input$method == "estimate_pi") {
+      system.time({ estimate_pi(B = input$B, seed = input$seed) })
+    } else {
+      system.time({ estimate_pi2(B = input$B, seed = input$seed) })
+    }
+  })
+  
+  output$plot <- renderPlot({
+    plot(simulate())
+  })
+  
+  output$time <- renderText({
+    # extract the time of the execution
+    paste("The time to run find the estimation of pi for the user is equal to",
+          round(simulated_time()[3], 4), "seconds."
+    )
+  })
+  
+  output$pi <- renderText({
+    # extract the estimated value
+    paste("The estimation for pi is equal to",
+          round(simulate()[[1]], 6)
+    )
+  })
 }
-
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+)
