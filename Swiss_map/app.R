a8ab76dfa2fd9b147ac8fda64b8aa377512bab9c
Luca Bron <lucagarrybron@gmail.com>
2018-12-05 19:37:42 +0100

    Shiny UI almost done.

--- Swiss_map/app.R
+++ Swiss_map/app.R
@@ -1,43 +1,75 @@
 library(shiny)
 library(leaflet)
 
-
 ui <- shinyUI(fluidPage(
-  leafletOutput("mymap"),
-  p(), #drity way of making space between map and button
-  actionButton("button", label = "Apply Changes", icon = NULL, width = NULL)
-))
+  leafletOutput("map"),
+  absolutePanel(top = 10, right = 10,
+                sliderInput("rangePrice", "Price", min(all_cities$price), max(all_cities$price),
+                            value = range(all_cities$price), step = 10
+                ),
+                sliderInput("rangeRooms", "Rooms", min(all_cities$rooms), max(all_cities$rooms),
+                            value = range(all_cities$rooms), step = 0.5
+                ),
+                sliderInput("rangeM2", "Surface in m\u00B2", min(all_cities$m2), max(all_cities$m2),
+                            value = range(all_cities$m2), step = 1
+                ),
+                selectInput("City", "City",
+                            unique(all_cities$city))
+                ),
+                hr(
+                ),
+                conditionalPanel(
+                  condition = "input.city == all_cities$city",
+                  selectInput("PostCode", "PostCode",
+                              "input.city" %>% unique(all_cities$postcode))
+                  )
+                )        
+                )#,
+                #checkboxInput("legend", "Show legend", TRUE) Might be handy later if we get legends.
+  )#,
+  # p(), #drity way of making space between map and button
+  # actionButton("button", label = "Apply Changes", icon = NULL, width = NULL)
+)
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
-  simulate <- eventReactive(input$button, {
+  points <- eventReactive(input$button, {
+    
+  }, ignoreNULL = FALSE)
   
-  output$mymap <- renderLeaflet(options = leafletOptions(minZoom = 7.4)) %>%
-    setMaxBounds(5.5, 48.2, 11, 45.3) %>%
-    addTiles() %>%  # Add default OpenStreetMap map tiles
-    addMarkers(
-      lng = all_cities$longitude,
-      lat = all_cities$latitude,
-      popup = paste(
-        "<b>Price :</b>",
-        all_cities$price,
-        "   CHF",
-        "<br/>",
-        "<b>Adress :</b>",
-        all_cities$address,
-        "<br/>",
-        "<b>Number of rooms :</b>",
-        all_cities$rooms,
-        "<br/>",
-        "<b>Size :</b>",
-        all_cities$m2,
-        " m2"
+  output$map <- renderLeaflet({
+    leaflet(all_cities, options = leafletOptions(minZoom = 7.4)) %>%
+      setMaxBounds(5.5, 48.2, 11, 45.3) %>%
+      addTiles() %>%  # Add default OpenStreetMap map tiles
+      addMarkers(
+        lng = all_cities$longitude,
+        lat = all_cities$latitude,
+        popup = paste(
+          "<b>Price :</b>",
+          all_cities$price,
+          "   CHF",
+          "<br/>",
+          "<b>Adress :</b>",
+          all_cities$address,
+          "<br/>",
+          "<b>Number of rooms :</b>",
+          all_cities$rooms,
+          "<br/>",
+          "<b>Size :</b>",
+          all_cities$m2,
+          " m2"
+        )
+      ) %>%
+      addPopups(
+        6.6322734, 46.5196535, popup = "Lausanne",
+        options = popupOptions(closeButton = T)
       )
-    )
   })
 }
 
 shinyApp(ui = ui, server = server)
+
+#### 
 
 ui <- shinyUI(fluidPage(
   
@@ -85,25 +117,34 @@ server <- shinyServer(function(input, output) {
   
   
   simulate <- eventReactive(input$button, {
-
     # simulate pi and measure the time here
-
-    # if (input$method == "estimate_pi") {
-    #   estimated_pi <- estimate_pi(B = input$B, seed = input$seed)
-    # } else {
-    #   estimated_pi <- estimate_pi2(B = input$B, seed = input$seed)
-    # }
-    # return(estimated_pi)
     
-    all_cities %>% filter(city == input$city, maxprice = input$price)
-    
-    
+    if (input$method == "estimate_pi") {
+      estimated_pi <- estimate_pi(B = input$B, seed = input$seed)
+    } else {
+      estimated_pi <- estimate_pi2(B = input$B, seed = input$seed)
+    }
+    return(estimated_pi)
     
   })
   
+  simulated_time <- eventReactive(input$button, {
+    if (input$method == "estimate_pi") {
+      system.time({ estimate_pi(B = input$B, seed = input$seed) })
+    } else {
+      system.time({ estimate_pi2(B = input$B, seed = input$seed) })
+    }
+  })
   
   output$plot <- renderPlot({
-    leafltet_my_df(simulate())
+    plot(simulate())
+  })
+  
+  output$time <- renderText({
+    # extract the time of the execution
+    paste("The time to run find the estimation of pi for the user is equal to",
+          round(simulated_time()[3], 4), "seconds."
+    )
   })
   
   output$pi <- renderText({
